5bfbbf228714140f63a3f83885c311555dccf72c
Danila Merzlyakov <merzlyakovdanila@gmail.com>
2023-03-30 13:13:55 +0300

    3-2. Download Image from internet

--- app/src/main/java/com/example/aston_dev_3_2/MainActivity.java
+++ app/src/main/java/com/example/aston_dev_3_2/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.aston_dev_3_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.aston_dev_3_2.databinding.ActivityMainBinding;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,7 +30,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private void loadImageFromURL(String url) {
+        Picasso.get()
+                .load(url)
+                .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
+                .error(R.drawable.image_not_available)
+                .fit()
+                .into(binding.imageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                    }
 
+                    @Override
+                    public void onError(Exception e) {
+                        toastShow("Ошибка: " + e.getMessage());
+                    }
+                });
     }
 
     @Override
@@ -32,7 +52,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (view.getId()) {
             case R.id.button:
-                loadImageFromURL(binding.editText.getText().toString());
+                if (!binding.editText.getText().toString().equals("")) {
+                    loadImageFromURL(binding.editText.getText().toString());
+                } else {
+                    toastShow(getString(R.string.error_empty_url));
+                }
         }
+    }
+
+    private void toastShow(String textToast) {
+        Toast toast = Toast.makeText(
+                this, textToast,
+                Toast.LENGTH_SHORT
+        );
+        toast.show();
     }
 }
